ef0c072cf8098ef33b1fda2bb2cf26d0dee87fc0
huitziuit <e.huitzilopochtli@gmail.com>
2021-12-03 18:38:06 -0600

    POST

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var  express = require('express');
 var app = express();
 const path = require('path')
 
+app.use(express.json());		//POST
+app.use(express.urlencoded());	//POST
+
+
 app.get('/',function(req,res){
 	//res.send('Hello World');
 	res.sendFile(path.join(__dirname+'/html/index.html'));
@@ -12,6 +16,10 @@ app.get('/',function(req,res){
 app.get('/saludo', function (req, res) {
 	res.send('Solicitud de saludo recibida');
 });
+
+app.post('/respuesta',(req,res)=>{
+	res.send("Nombre ingresado: " +req.body.nombre)
+})
 
 app.listen(8000, function(){
 	console.log('Servidor corriendo en el puerto 8000');
